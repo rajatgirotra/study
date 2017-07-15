73f19a56863a0eb7f9fc0bf9b8cce1b89c5e9133
Rajat Girotra <rajatgirotra@yahoo.com>
2017-07-15 15:33:24 +0800

    a

---
+++ pythonTutorial/numpy_pandas/7.py
@@ -0,0 +1,18 @@
+# free or universal functions
+import numpy as np
+
+# What about operations you want to do an the array but there is no operator for it. Example, taking cuberoot, square root etc. numpy provides free functions in the numpy module for that. Example: np.sqrt, np.exp
+# you have binary functions also which take two ndarrays and apply the operation binary operation element wise.
+# example np.maximum(x, y)
+
+arr = np.arange(24).reshape(6,4)
+print 'Original array\n %s' % arr
+print 'arr.sqrt\n %s' % np.sqrt(arr)
+print 'arr.exp\n %s' % np.exp(arr)
+
+
+arr1 = np.random.randn(9).reshape(3,3)
+arr2 = np.random.randn(9).reshape(3,3)
+print 'Original array 1\n %s' % arr1
+print 'Original array 2\n %s' % arr2
+print 'np.maximum(arr1, arr2)\n %s' % np.maximum(arr1, arr2)
