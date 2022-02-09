7537bb233f610000f6356ef33032bbe6b0a3973f
Rajat Girotra <rajatgirotra@yahoo.com>
2022-02-09 18:45:19 +0800

    s

--- cpp/CPP_NEW/CPP_17_20/inline_variables.hpp
+++ cpp/CPP_NEW/CPP_17_20/inline_variables.hpp
@@ -29,4 +29,53 @@ inline int X::m_tag {100};
 
 /* let's include this header both in inline_function.cpp and inline_functions_16.cpp and see that there are no compiler and linker errors.
     One more thing, in C++17, constexpr static data members are inline by default. That means we can omit out the line of definition of static constexpr variables, but not static const.
+	
  */
+ 
+ /*
+ In C++11, 
+ 1) non-static data members of any type,
+ 2) static constexpr data members of any type,
+ 3) and static const data members of integral or enumeration type may be initialized in the class declaration. e.g.
+
+   struct X {
+      int i=5;  --> 1)
+      const float f=3.12f;  --> 1)
+      static const int j=42; --> 3)
+      static constexpr float g=9.5f; --> 2)
+    };
+	
+Since float and double are not of integral or enumeration type, such members must either be constexpr,
+or non-static in order for the initializer in the class definition to be permitted.
+
+Prior to C++11, only static const data members of integral or enumeration type could have initializers in the class definition.
+i.e only 3) was allowed.
+
+Also if:
+
+In first.hpp
+template<typename T>
+struct cond_I
+{ static constexpr T value = 0; }; 
+
+
+// specialization 
+template<typename T>
+struct cond_I< std::complex<T> >
+{ static constexpr std::complex<T> value = {0,1}; }; 
+
+in main.cpp
+cout << cond_I<double>::value << endl;            // this works fine
+cout << cond_I< complex<double> >::value << endl; // linker error ?? with C++11 and C++14, not with C++17.
+
+linker error with C++11 and C++14 is because the ostream operator for std::complex<> takes the std::complex<> by reference. and that is
+considered odr-used. and for odr-used we must provide a definition.
+ostream::operator<<(ostream&, const complex<T> &) --> odr-used
+ostream::operator<<(ostream&, double); --> not odr-used
+
+with C++17, static constexpr is implicitly inline by default, so we dont need a definition.
+
+Also if we change static constexpr to static const, we get an error: as you read above only static const integral and enumeration types can have in class initializer. So here we have type T and type complex<>; both of which are not integral.
+
+
+*/
