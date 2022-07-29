598ee90a143c263a0f8be21b4dc2b17c115d2c19
Rajat Girotra <rajatgirotra@yahoo.com>
2022-07-29 09:13:53 +0800

    a

---
+++ interview/algorithms/crtcib/count_one_bits_in_integer.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int countSetBits(size_t N) {
+    int res = 0;
+     while (N != 0) {
+         if(N & 0x01) {
+             ++res;
+         }
+         N >>= 1;
+     }
+     return res;
+}
+
+int countSetBitsOptimized(size_t N) {
+    int res = 0;
+    while( N != 0 ) {
+        N &= (N - 1);
+        ++res;
+    }
+    return res;
+}
+
+int main() {
+    for(int i : {0, 1, 2, 3, 4, 5, 6, 7, 8, 9})  {
+      cout << "countSetBits(" << i << "): " << countSetBits(i) << endl;
+    }
+
+    for(int i : {0, 1, 2, 3, 4, 5, 6, 7, 8, 9})  {
+        cout << "countSetBitsOptimized(" << i << "): " << countSetBitsOptimized(i) << endl;
+    }
+}
