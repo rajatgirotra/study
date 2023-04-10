6eab09db5345009fb65d6e3e05fae4d4ffeeda5c
Rajat Girotra <rajatgirotra@yahoo.com>
2023-04-10 18:31:34 +0800

    study

--- cpp/CPP_NEW/algorithms/max_contiguous_subarray_size_k.cpp
+++ cpp/CPP_NEW/algorithms/max_contiguous_subarray_size_k.cpp
@@ -16,21 +16,59 @@
 using namespace std;
 
 template <auto N>
-auto max_contiguous_subarr_k(std::array<int, N> arr, int k) {
+auto max_contiguous_subarr_k_brute_force(std::array<int, N> arr, int k) {
+    // There will be N-k+1 sub arrays of size k.
+    // visit every subarray and keep count of maximum
+    assert(k <= N);
+    auto maxSum = std::numeric_limits<int>::min();
+    auto res = std::make_pair(-1, -1);
+    for(int i = 0; i < N-k+1; ++i) {
+        auto tmpSum{0};
+        auto j{i};
+        for(j = i; j < k + i; ++j) {
+            tmpSum += arr[j];
+        }
+        if(tmpSum > maxSum) {
+            maxSum = tmpSum;
+            res = std::make_pair(i, i+k-1);
+        }
+    }
+    return res;
 }
+
+template <auto N>
+auto max_contiguous_subarr_k_optimized(std::array<int, N> arr, int k) {
+    // There will be N-k+1 sub arrays of size k.
+    // we will find the first subarray sum and store that as result.
+    // then for every other subarray we will remove the first element and add the current element
+    assert(k <= N);
+    auto maxSum = std::accumulate(begin(arr), std::next(begin(arr), k), 0);
+    auto runningSum = maxSum;
+    auto res = std::make_pair(0, k-1);
+    for(int i = k; i < N; ++i) {
+        runningSum = runningSum - arr[i-k] + arr[i];
+        if(runningSum > maxSum) {
+            maxSum = runningSum;
+            res = std::make_pair(i-k+1, i);
+        }
+    }
+    return res;
+}
+
 
 int main() {
 
     std::random_device r;
     std::default_random_engine engine(r());
     std::uniform_int_distribution dist(-9, 9);
-    std::array<int, 8> arr;
+    std::array<int, 8> arr{5, 9, 9, -5, 6, 8, -7, 0};
 
     auto gen = [&]() {
         return dist(engine);
     };
 
-    std::generate(begin(arr), end(arr), gen);
+    (void)gen;
+//    std::generate(begin(arr), end(arr), gen);
 
     cout <<"Input array: ";
     std::copy(begin(arr), end(arr), ostream_iterator<int>(cout, " "));
@@ -38,6 +76,9 @@ int main() {
 
     int k = 4;
 
-    auto res = max_contiguous_subarr_k(arr, k);
+    auto res = max_contiguous_subarr_k_brute_force(arr, k);
     cout << "max_sum is from index " << res.first << " to " << res.second << ": " << std::accumulate(begin(arr)+res.first, begin(arr)+res.first+k, 0) << endl;
+
+    auto res2 = max_contiguous_subarr_k_optimized(arr, k);
+    cout << "max_sum is from index " << res2.first << " to " << res2.second << ": " << std::accumulate(begin(arr)+res2.first, begin(arr)+res2.first+k, 0) << endl;
 }
