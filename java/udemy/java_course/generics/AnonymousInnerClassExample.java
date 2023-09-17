2492aba3632ad8f0ac5ebee564556df81d3a9ad4
Rajat Girotra <rajatgirotra@yahoo.com>
2023-09-17 22:34:50 +0800

    js

---
+++ java/udemy/java_course/generics/AnonymousInnerClassExample.java
@@ -0,0 +1,46 @@
+/*
+ * An anonymous inner class is a class which is created inside a function and you
+ * 1) will only ever create one object of; not more than one
+ * 2) the class is defined together with the object creation in the same java statement
+ * 3) the anonymous inner class can extend some class or implement some interface.
+ * 4) You create the anonymous inner class simple by creating an object of a normal class, followed by the class body inside {}. See below.
+ */
+
+package generics;
+
+class Animal {
+    public void makeNoise() {
+        System.out.println("Generic animal cannot make a noise");
+    }
+}
+
+
+
+public class AnonymousInnerClassExample {
+    public static void main(String[] args) {
+        Animal generic = new Animal(); // regular Animal object
+        generic.makeNoise();
+
+        // anonymous inner class extending Animal
+        Animal dog = new Animal() {
+            @Override
+            public void makeNoise() {
+                System.out.println("Woof Woof!!");
+            }
+        };
+        System.out.println("dog variable class type is: " + dog.getClass().getName());
+
+
+        // anonymous inner class implementing Runnable
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("I am an anonymous runnable");
+            }
+        };
+        System.out.println("runnable variable class type is: " + runnable.getClass().getName());
+
+        dog.makeNoise();
+        runnable.run();
+    }    
+}
---
+++ java/udemy/java_course/multithreading/1_README.txt
@@ -0,0 +1,30 @@
+Ways to create threads in java.
+
+1st Way
+========
+Just extend from Thread and override the run method. Later call the start() function on your derived class. Note that calling the run() function will 
+not start a new thread. calling thread's start() function will install all the machinery for creating and starting a new thread.
+
+class CounterThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("new thread starting..");
+    }
+}
+CounterThread ct = new CounterThread();
+ct.start();
+
+2nd Way
+========
+Just implement the Runnable interface and override the run method. Later need to create a Thread object and pass this class as a Runnable. Then as before
+call start() function on the thread object.
+
+class CounterThread implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("new thread starting..");
+    }
+}
+CounterThread ct = new CounterThread();
+Thread someObj = new Thread(ct);
+ct.start();
